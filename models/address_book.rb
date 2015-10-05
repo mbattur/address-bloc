0dbd5416cd77046f9127b51b14f359cddc73fcc9
mbattur <mbattur@yahoo.com>
2015-10-05 22:57:53 +0000

    Assignment 21 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -17,4 +17,8 @@ class AddressBook
         end
         @entries.insert(index, Entry.new(name, phone_number, email))
     end
+    
+    def remove_entry(name, phone_number)
+        
+    end
 end
